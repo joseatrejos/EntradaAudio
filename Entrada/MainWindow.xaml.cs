21acb548f21f354fc7a26ec28bf3c49daa86ff72
cecy castañeda <ceciliacast00@hotmail.com>
2019-04-05 12:31:31 -0700

    Se vinculó el slider con el volumen de las muestras grabadas.

--- Entrada/MainWindow.xaml.cs
+++ Entrada/MainWindow.xaml.cs
@@ -41,6 +41,7 @@ namespace Entrada
                 // Ahora agregamos a los "items" de nuestro combo box el Nombre del dispositivo almacenado en capacidades[i]
                 cmb_Dispositivos.Items.Add(capacidades.ProductName);
             }
+            cmb_Dispositivos.SelectedIndex = 0;
         }
 
         private void btn_Iniciar_Click(object sender, RoutedEventArgs e)
@@ -53,7 +54,39 @@ namespace Entrada
             // Buffer
             waveIn.BufferMilliseconds = 250;
 
-            // 
+            // ¿Qué hacer cuando hay muestras disponibles?
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+
+            waveIn.StartRecording();
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] buffer = e.Buffer;
+            int bytesGrabados = e.BytesRecorded;
+            float acumulador = 0.0f;
+
+            // Aquí se transforman 2 bytes separados en una muestra de 16 bits
+            for (int i = 0; i < bytesGrabados; i += 2)
+            {                                           // Este "OR" revisa ambos parámetros para ver si se cumple la condición
+                short muestra = (short)(buffer[i + 1] << 8 | buffer[i]);
+
+                /* 1. Se toma el segundo byte y anteponemos 8 ceros al principio
+                   2. Se hace un OR (matemática discreta) con el primer byte, al cual automáticamente se le llenan los 8 ceros 
+                   del principio con los últimos 8 valores del segundo byte */
+
+                float muestra32bits = (float)(muestra/32768.0f);
+                acumulador += Math.Abs(muestra32bits);
+            }
+            /* Puesto que cada muestra son 2 bytes, divides entre 2 el número de bytes grabados para dividir lo acumulado entre
+               el número de muestras (para obtener el promedio) */
+            float promedio = acumulador / (bytesGrabados / 2.0f);
+            sld_Microfono.Value = (double)promedio;
+        }
+
+        private void btn_Detener_Click(object sender, RoutedEventArgs e)
+        {
+            waveIn.StopRecording();
         }
     }
 }
